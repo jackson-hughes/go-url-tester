550bdd6c2b7f833fb5916e1060c5987d02d1e09f
Jackson Hughes <jxhughes95@gmail.com>
2019-05-23 22:37:53 +0100

    Add concurrency!

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 )
+
+func testUrl() {
+
+}
 
 func main() {
 
@@ -13,14 +18,22 @@ func main() {
 		log.Fatal("No input provided, please provide at least one url to test")
 	}
 
+	numUrls := len(os.Args[1:])
+
+	var wg sync.WaitGroup
+	wg.Add(numUrls)
+
 	for _, i := range os.Args[1:] {
+		go func(i string) {
+			defer wg.Done()
+			r, err := http.Get(i)
 
-		r, err := http.Get(i)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("url: "+i, "\t"+r.Status)
+			fmt.Println("url: "+i, "\t"+r.Status)
+		}(i)
 	}
+	wg.Wait()
 }
